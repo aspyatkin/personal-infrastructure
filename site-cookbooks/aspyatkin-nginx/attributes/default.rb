7432bae8275700aa3e9cef8f77c154b0b47de76d
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-10-03 01:45:22 +0400

    Fixes

--- site-cookbooks/aspyatkin-nginx/attributes/default.rb
+++ site-cookbooks/aspyatkin-nginx/attributes/default.rb
@@ -1,13 +1,16 @@
-default['nginx']['version'] = '1.9.5'
-default['nginx']['install_method'] = 'source'
-default['nginx']['default_site_enabled'] = false
-default['nginx']['source']['version'] = '1.9.5'
-default['nginx']['source']['modules'] = [
+default['aspyatkin-nginx']['ssl_stapling'] = nil
+default['aspyatkin-nginx']['ssl_dhparam'] = nil
+
+override['nginx']['version'] = '1.9.5'
+override['nginx']['install_method'] = 'source'
+override['nginx']['default_site_enabled'] = false
+override['nginx']['source']['modules'] = [
   'nginx::http_gzip_static_module',
   'nginx::http_ssl_module',
   'nginx::ipv6',
   'aspyatkin-nginx::http_v2_module'
 ]
-default['nginx']['source']['checksum'] = '48e2787a6b245277e37cb7c5a31b1549a0bbacf288aa4731baacf9eaacdb481b'
-default['nginx']['server_tokens'] = 'off'
-default['nginx']['ssl_stapling'] = false
+override['nginx']['source']['checksum'] = '48e2787a6b245277e37cb7c5a31b1549a0bbacf288aa4731baacf9eaacdb481b'
+override['nginx']['server_tokens'] = 'off'
+
+node.from_file(run_context.resolve_attribute('nginx', 'source'))
