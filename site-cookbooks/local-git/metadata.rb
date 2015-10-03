66c9cc37083d356bfb4d7e87cf6cd2d6300f6f9d
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-10-03 17:15:39 +0400

    Wrap nodejs cookbook and rename things

--- site-cookbooks/local-git/metadata.rb
+++ site-cookbooks/local-git/metadata.rb
@@ -1,6 +1,6 @@
-name 'aspyatkin-git'
+name 'local-git'
 description 'Installs and configures git'
 version '1.0.0'
 
-recipe 'aspyatkin-git', 'Installs and configures git'
+recipe 'local-git', 'Installs and configures git'
 depends 'git', '4.3.4'
--- site-cookbooks/local-nginx/attributes/default.rb
+++ site-cookbooks/local-nginx/attributes/default.rb
@@ -1,5 +1,5 @@
-default['aspyatkin-nginx']['ssl_stapling'] = nil
-default['aspyatkin-nginx']['ssl_dhparam'] = nil
+default['local-nginx']['ssl_stapling'] = nil
+default['local-nginx']['ssl_dhparam'] = nil
 
 override['nginx']['version'] = '1.9.5'
 override['nginx']['install_method'] = 'source'
@@ -8,7 +8,7 @@ override['nginx']['source']['modules'] = [
   'nginx::http_gzip_static_module',
   'nginx::http_ssl_module',
   'nginx::ipv6',
-  'aspyatkin-nginx::http_v2_module'
+  'local-nginx::http_v2_module'
 ]
 override['nginx']['source']['checksum'] = '48e2787a6b245277e37cb7c5a31b1549a0bbacf288aa4731baacf9eaacdb481b'
 override['nginx']['server_tokens'] = 'off'
