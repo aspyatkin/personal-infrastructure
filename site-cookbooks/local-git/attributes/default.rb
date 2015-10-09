7511c2ed5b9f49fb308d31d63c02f5c368b63d35
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-10-09 11:30:53 +0400

    Correct git tarball checksum

--- site-cookbooks/local-git/attributes/default.rb
+++ site-cookbooks/local-git/attributes/default.rb
@@ -1,4 +1,4 @@
 override['git']['version'] = '2.6.0'
-override['git']['checksum'] = '3ffdd9072dc16d81f36988f6fa00c7cf3b7ad50decbbf8a3e75df54b8bc967a6'
+override['git']['checksum'] = '807ebf4ea8a96a9a7e48be5101fb23e31135c1a084b041a25f9a5a76297d8140'
 
 node.from_file(run_context.resolve_attribute('git', 'default'))
