453f14db0310c779d023cf1f1f40d9ecd9dbf706
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-10-17 14:41:58 +0400

    Use latest-git cookbook

--- site-cookbooks/aspyatkin.com/metadata.rb
+++ site-cookbooks/aspyatkin.com/metadata.rb
@@ -3,7 +3,7 @@ description 'Installs and configures aspyatkin.com'
 version '1.0.0'
 
 recipe 'aspyatkin.com', 'Installs and configures aspyatkin.com'
-depends 'local-git', '1.0.0'
+depends 'latest-git', '1.0.0'
 depends 'local-nginx', '1.0.0'
 depends 'local-nodejs', '1.0.0'
 depends 'local-rbenv', '1.0.0'
