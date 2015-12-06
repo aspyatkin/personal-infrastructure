0418b71fd914ae8d9baa75924b7b3b24f6aa5847
Alexander Pyatkin <aspyatkin@users.noreply.github.com>
2015-12-06 21:38:36 +0400

    Mount letsencrypt webroot and some other minor changes

--- site-cookbooks/aspyatkin.com/recipes/default.rb
+++ site-cookbooks/aspyatkin.com/recipes/default.rb
@@ -4,19 +4,13 @@ include_recipe 'local-nginx'
 include_recipe 'local-rbenv'
 include_recipe 'dotfiles'
 
-directory '/var/www' do
-    owner node[:app][:user]
-    group node[:app][:group]
-    mode '0755'
-    action :create
-end
-
 base_dir = "/var/www/#{node[:app][:domain]}"
 
 directory base_dir do
     owner node[:app][:user]
     group node[:app][:group]
-    mode '0755'
+    mode 0755
+    recursive true
     action :create
 end
 
@@ -29,50 +23,79 @@ git base_dir do
     action :sync
 end
 
+letsencrypt_root = '/var/www/letsencrypt'
+letsencrypt_dir = ::File.join letsencrypt_root, '.well-known', 'acme-challenge'
+
+directory letsencrypt_dir do
+    owner 'root'
+    group node['root_group']
+    mode 0755
+    recursive true
+    action :create
+end
+
+node_env = 'development'
+if node[:app].has_key? 'environment'
+    node_env = node[:app][:environment]
+end
+
+data_bag_item('letsencrypt', node_env)['dv'].each do |key, value|
+    path = ::File.join letsencrypt_dir, key
+    file path do
+        owner 'root'
+        group node['root_group']
+        mode 0644
+        content value
+        action :create
+    end
+end
+
 template "#{node[:nginx][:dir]}/sites-available/#{node[:app][:domain]}.conf" do
     source 'nginx.conf.erb'
-    mode '0644'
+    mode 0644
     notifies :reload, 'service[nginx]', :delayed
+    action :create
 end
 
 template "#{node[:nginx][:dir]}/conf.d/ssl.conf" do
     source 'ssl.conf.erb'
-    mode '0644'
+    mode 0644
     notifies :reload, 'service[nginx]', :delayed
+    action :create
 end
 
 nginx_site "#{node[:app][:domain]}.conf"
 
 directory '/var/ssl' do
     owner 'root'
-    group 'root'
-    mode '0700'
+    group node['root_group']
+    mode 0700
     action :create
 end
 
 file "/var/ssl/#{node[:app][:domain]}.crt" do
     owner 'root'
-    group 'root'
-    mode '0600'
+    group node['root_group']
+    mode 0600
+    content data_bag_item('ssl', node_env)['crt']
     action :create
-    content data_bag_item('ssl', node[:app][:domain])['crt']
 end
 
 file "/var/ssl/#{node[:app][:domain]}.key" do
     owner 'root'
-    group 'root'
-    mode '0600'
+    group node['root_group']
+    mode 0600
+    content data_bag_item('ssl', node_env)['key']
     action :create
-    content data_bag_item('ssl', node[:app][:domain])['key']
 end
 
 if node['local-nginx'].has_key? 'ssl_stapling' and node['local-nginx'][:ssl_stapling]
     file "/var/ssl/#{node[:app][:domain]}-trusted.crt" do
         owner 'root'
-        group 'root'
-        mode '0600'
+        group node['root_group']
+        mode 0600
+        content data_bag_item('ssl', node_env)['trusted_certs']
         action :create
-        content data_bag_item('ssl', node[:app][:domain])['trusted_certs']
     end
 end
 
